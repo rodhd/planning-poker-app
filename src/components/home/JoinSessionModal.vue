cd4e249f1141e6e4d3bed890afa2521ff754abfb
Rodrigo H <46557955+rodhd@users.noreply.github.com>
2022-01-29 19:08:11 +0100

    clean up and decorate

---
+++ src/components/home/JoinSessionModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-show="isActive" class="fixed inset-0 w-full h-screen flex items-center justify-center bg-black/75">
+    <div class="w-full max-w-2xl bg-blue-300 shadow-lg rounded-lg p-8">
+      <input 
+        class="
+          mt-1
+          block
+          w-full
+          rounded-md
+          bg-gray-100
+          border-transparent
+          focus:border-gray-500 focus:bg-white focus:ring-0"
+        type="text"
+        placeholder="Enter code"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+
+export default defineComponent({
+  props: {
+    isActive: Boolean
+  }
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,44 @@
 <template>
   <div class="flex items-center justify-center h-screen">
-    <div>
-      <button 
-        class="py-4 px-2 bg-red-300 border-gray-700 shadow rounded"
-        @click="createSession">
-          Create New Session
-      </button>
+    <div class="py-3 px-6 bg-blue-100 rounded shadow-md">
+      <h1 class="text-2xl font-semibold">Planing Poker App</h1>
+      <p>Create or join a new session</p>
+      <div class="flex flex-row mt-3">
+        <button
+          class="
+            py-4 
+            px-2 
+            mr-2 
+            bg-red-500 
+            border-gray-700 
+            text-white
+            font-semibold
+            min-w-fit 
+            w-1/2 
+            shadow 
+            rounded
+            hover:bg-red-300"
+          @click="createSession">
+            New Session
+        </button>
+        <button
+          class="
+            py-4 
+            px-2 
+            bg-gray-500 
+            border-gray-700
+            text-white
+            font-semibold
+            min-w-fit 
+            w-1/2 
+            shadow 
+            rounded
+            hover:bg-gray-300"
+          @click="toggleModal">
+            Join Session
+        </button>
+      </div>
+      <JoinSessionModal :isActive="modalToggled" />
     </div>
   </div>
 </template>
@@ -13,17 +46,24 @@
 <script lang="ts">
 import { createSession } from '@/api/sessions';
 import { Options, Vue } from 'vue-class-component';
+import JoinSessionModal from '@/components/home/JoinSessionModal.vue';
 
 @Options({
   components: {
+    JoinSessionModal
   },
 })
 export default class HomeView extends Vue {
   sessionId: string | null = null;
+  modalToggled = false;
 
   async createSession() {
     const session = await createSession();
     this.$router.push({ name: 'session', params: { sessionId: session.sessionId } });
+  }
+
+  toggleModal() {
+    this.modalToggled = !this.modalToggled;
   }
 }
 </script>
